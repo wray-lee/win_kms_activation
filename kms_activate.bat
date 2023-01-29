4b6aef5b6d766b7c03ddebf56cf60e04accf3169
Wray Lee <79258616+wray-lee@users.noreply.github.com>
2023-01-29 23:17:16 +0800

    Update kms_activate.bat

--- kms_activate.bat
+++ kms_activate.bat
@@ -2,14 +2,14 @@ slmgr /upk
 ::show system information
 slmgr /dlv
 ::
-set /p _key_=supply your activation key according to the winver showed previously
+set /p _key_=supply your activation key according to the winver showed previously:
 slmgr /ipk "%_key_%"
-set /p var=change customized kms server or not(y/n).
+set /p var=change customized kms server or not(y/n):
 if /I "%var%"=="y" goto kms_server
 if /I "%var%"=="n" goto no_kms
 
 :kms_server
-set /p _server_=supply your kms server
+set /p _server_=supply your kms server:
 goto A1
 
 :no_kms
